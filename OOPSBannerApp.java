ee020eae39d06071449a930fec70eb5a08520b5f
Shweta <hs0866@srmist.edu.in>
2026-03-01 20:45:40 +0530

    Improved alignment in UC3 using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,35 @@
 /**
  * OOPSBannerApp
  *
- * UC2: Prints the word "OOPS" in large banner format using * and spaces.
+ * UC3: Prints the word "OOPS" in large banner format using String.join().
  *
  * @author Shweta
- * @version 2.0
+ * @version 3.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****     *****     ******    ******  ");
-        System.out.println(" *     *   *     *   *      *  *      * ");
-        System.out.println("*       * *       *  *      *  *        ");
-        System.out.println("*       * *       *  ******    ******    ");
-        System.out.println("*       * *       *  *         *         ");
-        System.out.println(" *     *   *     *   *         *      *  ");
-        System.out.println("  *****     *****     *          ******    ");
+        System.out.println(String.join(" ",
+                " *****  ", " *****  ", " ****** ", " ****** "));
+
+        System.out.println(String.join(" ",
+                "*     * ", "*     * ", "*     * ", "*      "));
+
+        System.out.println(String.join(" ",
+                "*     * ", "*     * ", "*     * ", "*      "));
+
+        System.out.println(String.join(" ",
+                "*     * ", "*     * ", "******  ", " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     * ", "*     * ", "*       ", "      *"));
+
+        System.out.println(String.join(" ",
+                "*     * ", "*     * ", "*       ", "      *"));
+
+        System.out.println(String.join(" ",
+                " *****  ", " *****  ", "*       ", " ***** "));
     }
 }
